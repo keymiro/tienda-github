270381456e6c5d7a649e2742f901c9597c9ef879
keymiro <camilomancipe@outlook.com>
2019-04-23 21:14:35 -0500

    Primer commit

--- database/migrations/2019_10_12_000000_create_users_table.php
+++ database/migrations/2019_10_12_000000_create_users_table.php
@@ -16,6 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('apellidos');
+            $table->string('documento');
+            $table->string('direccion');
+            $table->date('fechanacimiento');
+            $table->string('telefono');
+            $table->unsignedBigInteger('persona_id');
+            $table->foreign('persona_id')->references('id')->on('personas');
+            $table->unsignedBigInteger('rol_id');
+            $table->foreign('rol_id')->references('id')->on('roles');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
