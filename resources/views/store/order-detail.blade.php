c981fc419e6674fc1c7c1ba1f68f3eb59ca3eb75
keymiro <camilomancipe@outlook.com>
2019-04-30 13:04:13 -0500

    Detalle producto terminado

--- resources/views/store/order-detail.blade.php
+++ resources/views/store/order-detail.blade.php
@@ -14,10 +14,12 @@
                 <table class="table table-striped table-hover ">
                     <tr><td>Nombre:</td><td>{{ Auth::user()->name . " " . Auth::user()->last_name }}</td></tr>
                     <tr><td>Usuario:</td><td>{{ Auth::user()->apellidos }}</td></tr>
+                    <tr><td>Documento:</td><td>{{ Auth::user()->documento }}</td></tr>
                     <tr><td>Correo:</td><td>{{ Auth::user()->email }}</td></tr>
                     <tr><td>Dirección:</td><td>{{ Auth::user()->direccion }}</td></tr>
                 </table>
             </div>
+            <hr>
             <div class="table-responsive">
                 <h3>Datos del pedido</h3>
                 <table class="table table-striped table-hover table-bordered">
@@ -36,6 +38,22 @@
                         </tr>
                     @endforeach
                 </table>
+                <hr>
+                <h3>
+					<span class="badge badge-success">
+						Total: ${{ number_format($total, 2) }}
+					</span>
+                </h3>
+                <hr>
+                <p>
+                    <a href="{{ route('cart-show') }}" class="btn btn-primary">
+                        <i class="fa fa-chevron-circle-left"></i> Regresar
+                    </a>
+
+                    <a href="#" class="btn btn-warning">
+                        Pagar con <i class="fa fa-cc-paypal fa-2x"></i>
+                    </a>
+                </p>
             </div>
         </div>
     </div>
